330329ac873c9b9b802a64d320a768d9a81d4d62
mhelmkampf <martin.helmkampf@leibniz-zmt.de>
2024-03-15 11:37:48 +0100

    initial commit

---
+++ figures/Fig1b_ftol.R
@@ -0,0 +1,147 @@
+### ============================================================================
+### Hamlet phylogeny: R code to plot Fig. 1b (Serraninae FToL phylogeny)
+### Created Jan 2024 by Martin Helmkampf
+### ============================================================================
+
+
+### Preparations
+library(ape)
+library(treeio)
+library(ggtree)
+library(tidyverse)
+
+dev.off()
+rm(list=ls())
+
+
+### Read in and root tree
+phylo <- read.tree(paste("../data/trees/ftol_v2.treefile"))
+
+
+### Root tree
+rooted <- root(phy = phylo, outgroup = "Epinephelus_maculatus", edgelabel = TRUE)
+
+
+### Edit tip labels
+rooted$tip.label <- rooted$tip.label %>% 
+  str_replace(pattern = "PL17_88abebel", "H. aberrans") %>%
+  str_replace(pattern = "19174affgun", "H. affinis") %>%
+  str_replace(pattern = "54786atlliz", "H. atlahua") %>%
+  str_replace(pattern = "54649casliz", "H. castroaguirrei") %>%
+  str_replace(pattern = "PL17_38chlpri", "H. chlorurus") %>%
+  str_replace(pattern = "62576ecoarc", "H. ecosur") %>%
+  str_replace(pattern = "62558floarc", "H. floridae") %>%
+  str_replace(pattern = "62570gemarc", "H. gemma") %>%
+  str_replace(pattern = "23301gumboc", "H. gummigutta") %>%
+  str_replace(pattern = "19076gutbar", "H. guttavarius") %>%
+  str_replace(pattern = "PL17_64indpri", "H. indigo") %>%
+  str_replace(pattern = "HypoHaiti1libhai", "H. liberte") %>%
+  str_replace(pattern = "PL17_122maybel", "H. maya") %>%
+  str_replace(pattern = "18906nigboc", "H. nigricans") %>%
+  str_replace(pattern = "20650prohon", "H. providencianus") %>%
+  str_replace(pattern = "19104puegun", "H. puella") %>%
+  str_replace(pattern = "FL0880ransan", "H. randallorum") %>%
+  str_replace(pattern = "PL17_72tanpri", "H. sp. (tan)") %>%
+  str_replace(pattern = "PL17_136uniflk", "H. unicolor") %>%
+  str_replace(pattern = "20480tabhon", "Se. tabacarius") %>%
+  str_replace(pattern = "PL17_21tigboc", "Se. tigrinus") %>%
+  str_replace(pattern = "Bocas16.3torboc", "Se. tortugarum") %>% 
+#
+  str_replace(pattern = "([A-Z])([a-z])[a-z]*_([a-z]*)", "\\1\\2. \\3") %>%
+  str_replace(pattern = "Za.", "Pl.") %>%
+  str_replace(pattern = "Hy.", "H.") %>% 
+  str_replace(pattern = "Di.", "D.") %>% 
+  str_replace(pattern = "Ep.", "E.")
+
+
+### Identify long branches (requires ggtree object index)
+tree_data <- ggtree(rooted)$data
+longg <- which(tree_data$branch.length > mean(tree_data$branch.length) * 10)
+
+
+### Compress long branches (requires rooted phylo object index)
+# longr <- which(rooted$edge.length > mean(rooted$edge.length) * 10)
+# rooted$edge.length[longr] <- rooted$edge.length[longr] * 0.5
+
+
+### Define groups
+hamlets <- rooted$tip.label[grepl(pattern = "H. ", rooted$tip.label)]
+our_taxa <- c(hamlets, "Se. tabacarius", "Se. tigrinus", "Se. tortugarum")
+
+
+### Add species / location labels and support categories
+(tree <- ggtree(rooted, 
+                layout = "rectangular", 
+                ladderize = TRUE, 
+                right = TRUE) %>%
+    .$data %>%
+    mutate(support = as.numeric(if_else(!isTip, label, "NA")),
+           support_class = cut(as.numeric(support), c(0,50,70,90,100)) %>%
+             as.character() %>% factor(levels = c("(0,50]", "(50,70]", "(70,90]", "(90,100]"))) %>%
+           # branch_type = case_when(node %in% longg ~ "broken", 
+           #                         TRUE ~ "whole")) %>%
+    groupOTU(.node = hamlets)
+)
+
+
+### Define neutral color
+clr_neutral <- rgb(0.2, 0.2, 0.2)
+
+
+### Draw tree
+(t <- ggtree(tree,
+             aes(color = clr_neutral,
+                 # linetype = branch_type
+                 )) +
+  geom_tiplab(aes(color = group, label = if_else(label %in% our_taxa, str_c(label,"*"), label)),   # add asterisks to our taxa
+              size = 3.5, 
+              hjust = -0.1,
+              fontface = 'italic') +
+  ggplot2::xlim(0, 0.17) +   # add extra space for long labels
+  geom_nodepoint(aes(fill = support_class,
+                     size = support_class),
+                     shape = 21) +
+  # labs(title = "Serraninae, updated FToL phylogeny",
+  #     subtitle = "23 nuclear and mt genes, partition model, IQ-TREE, 200 non-parametric bootstraps") +
+  scale_color_manual(values = c(clr_neutral, clr_neutral, "firebrick")) +
+  scale_fill_manual(values = c(`(0,50]`   = "transparent",
+                               `(50,70]`  = "white",
+                               `(70,90]`  = "gray",
+                               `(90,100]` = "black"),
+                    breaks = c("(0,50]", "(50,70]", "(70,90]", "(90,100]"),
+                    drop = FALSE) +
+  scale_size_manual(values = c(`(0,50]`   = 0,
+                               `(50,70]`  = 1.5,
+                               `(70,90]`  = 1.5,
+                               `(90,100]` = 1.5),
+                    breaks = c("(0,50]", "(50,70]", "(70,90]", "(90,100]"),
+                    na.value = 0,
+                    drop = FALSE) +
+  scale_linetype_manual(values = c(whole = 1, broken = 3), guide = "none") +
+  geom_treescale(color = "gray", 
+                 fontsize = 3, 
+                 x = 0, 
+                 y = 15) +
+  guides(color = "none",
+         fill = guide_legend(title = "Support", title.position = "top"),
+         size = guide_legend(title = "Support", title.position = "top")
+         ) +
+  theme_void() +
+  theme(plot.margin =  margin(l = 10),
+        plot.title = element_text(size = 13, face = "bold", vjust = 3, margin = margin(15, 0, 5, 0)),
+        plot.subtitle = element_text(size = 11, margin = margin(0, 0, 30, 0)),
+        legend.position = c(0.05,0.05),
+        legend.justification = c(0,0),
+        legend.title.align = 0,
+        legend.text = element_text(color = clr_neutral),
+        legend.title = element_text(color = clr_neutral))
+  )
+
+
+### Save to file
+ggsave(plot = t,
+       filename = "Fig1b_ftol.pdf",
+       width = 8,
+       height = 6,
+       device = cairo_pdf,
+       bg = "transparent")
---
+++ figures/Fig2b_pi.R
@@ -0,0 +1,176 @@
+### ============================================================================
+### Hamlet phylogeny: R code to plot Fig. 2b (nucleotide diversity, tskit)
+### Created Jan 2024 by Martin Helmkampf
+### ============================================================================
+
+
+### Preparations
+library(tidyverse)
+library(matrixStats)
+library(ggpattern)
+library(cowplot)
+
+rm(list=ls())
+dev.off()
+
+
+### Define dataset
+ds <- "phylo2e"
+level <- "pop"
+lgs <- paste0("LG", sprintf('%0.2d', 1:24))
+
+
+### Declare populations
+set <- read_tsv("../data/samples/samples_phylo.txt", col_names = "Sample") %>%
+  mutate(Population = str_sub(Sample, -6, -1))
+# set <- set %>% filter(!grepl("tor|tab|tig", Population))   # execute if outgroups were removed
+
+populations <- levels(factor(set$Population))
+
+
+### Define regions
+gulf <- c("liz", "tam", "ala", "arc", "are", "flk")
+caribbean <- c("bel", "boc", "gun", "hon", "san", "qui")
+atlantic <- c("bar", "hai", "pri")
+
+
+### Read in data from files
+df <- data.frame(0)
+
+for (region in lgs) {
+  pi <- as.numeric(read_lines(file = paste0("../data/pi/pi_", ds, "_", region, "_", level, "_n1.txt")))
+  df <- cbind(df, pi)
+}
+
+df <- subset(df, select = -1)
+
+colnames(df) <- lgs
+
+
+### Calculate means and sd, add population and region ids
+nucdiv <- as_tibble(df) %>%
+  mutate(mean_pi = rowMeans(.),
+         sd_pi = rowSds(as.matrix(.))) %>%
+  select(mean_pi, sd_pi) %>%
+  cbind(populations) %>%
+  rename(population = populations) %>%
+  mutate(spec = str_sub(population, -6, -4),
+         loc = str_sub(population, -3, -1),
+         region = case_when(
+           loc %in% gulf ~ "Gulf of Mexico",
+           loc %in% caribbean ~ "Caribbean",
+           loc %in% atlantic ~ "Atlantic",
+           TRUE ~ "ungrouped"),
+         clade = case_when(
+           spec %in% c("atl", "eco", "flo") ~ "Small clade",
+           TRUE ~ "Large clade"
+         )
+  ) %>%
+  filter(!grepl("tor|tab|tig", population),
+         !population %in% set)
+
+
+### Create histogram plot
+(g <- nucdiv %>%
+    ggplot(aes(x = reorder(population, -mean_pi),
+               y = mean_pi,
+               fill = region,
+               pattern = clade)) +
+    geom_bar_pattern(position = "dodge",
+             stat = "identity",
+             color = "grey20",
+             alpha = 0.8,
+             lwd = 0.4,
+             width = 1,
+             pattern_color = "grey35",
+             pattern_fill = "grey35",
+             pattern_angle = 45,
+             pattern_density = 0.02,
+             pattern_spacing = 0.02,
+             pattern_key_scale_factor = 0.75) +
+    geom_errorbar(aes(ymin = mean_pi - sd_pi, ymax = mean_pi + sd_pi),
+                  color = "grey50",
+                  width = 0.4) +
+    scale_fill_manual(breaks = c("Gulf of Mexico", "Caribbean", "Atlantic"),
+                      values = c("coral2", "royalblue1", "olivedrab4")) +
+    scale_pattern_manual(breaks = c("Small clade", "Large clade"), 
+                         values = c("stripe", "none")) +
+    labs(# title = paste0("Mean genome-wide nucleotide diversity (π) per population"), 
+         x = NULL, 
+         y = expression(paste("Nucleotide diversity (", italic("\u03c0"), ")"))
+         ) +
+    guides(fill = "none",
+           pattern = guide_legend(title = "Clade", override.aes = list(fill = "white"))
+           ) +
+    theme_minimal() +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor = element_blank(),
+          plot.title = element_text(size = 12, margin = margin(0, 0, 5, 0)),
+          # axis.text.x = element_text(hjust = 1, vjust = 1, angle = 35, size = 7,
+          #                            margin = unit(c(-3, 0, 0, 0), "mm")),
+          axis.text.x = element_blank(),
+          axis.text.y = element_text(size = 16),
+          axis.title.y = element_text(size = 16, vjust = 3),
+          legend.position = c(0.78, 0.875), legend.box = "horizontal", legend.direction = "horizontal",
+          legend.title = element_blank(),
+          legend.text = element_text(size = 16),
+          plot.margin = unit(c(3, 3, 0, 3), "mm")
+          )
+)
+
+
+### Set species colors
+colstandard <- read_tsv("../data/samples/species_colors.tsv", 
+                        col_types = "cc", 
+                        col_names = TRUE)
+
+
+### Create species color table
+scol <- nucdiv %>%
+  arrange(-mean_pi) %>%
+  mutate(position = seq(1:length(nucdiv$population)))
+
+
+### Plot species colors
+(s <- ggplot(scol, aes(xmin = position, xmax = position + 1, 
+                       ymin = -Inf, ymax = Inf)) +
+  geom_rect(aes(fill = spec)) +
+    scale_x_discrete(breaks = seq(1:length(nucdiv$population))) +
+    scale_y_continuous(breaks = .5, limits = c(0, 1)) +
+    scale_fill_manual(values = colstandard$Color) +
+    labs(x = NULL, 
+         y = "Species") +
+    theme_minimal() +
+    theme(panel.grid = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          # axis.title.y = element_text(angle = 0, vjust = 0.5, 
+          #                             color = "gray20", size = 9,
+          #                             margin = margin(r = -40)),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          legend.position = "none",
+          plot.margin = unit(c(-3, 0, 3, 0), "mm")
+    )
+)
+
+
+### Combine plots
+(p_comb <- plot_grid(g, s, 
+                     align = "v", 
+                     nrow = 2, 
+                     rel_heights = c(0.93, 0.07),
+                     scale = c(1, 1.015))
+)
+
+
+### Save plot
+ggsave(
+  filename = paste0("Fig2b_pi.png"),
+  plot = p_comb,
+  width = 20,
+  height = 10,
+  units = "cm",
+  device = png,
+  bg = "white"
+)
