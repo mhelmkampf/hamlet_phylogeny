2458520eb8a7917dd7c66f7b93fc192105805a6f
Floriane Coulmance <10512994+florianecoulmance@users.noreply.github.com>
2024-05-16 23:37:56 +0200

    Add files via upload

--- code/R/gxp_plot.R
+++ code/R/gxp_plot.R
@@ -56,15 +56,17 @@ prep_file <- function(f,path) {
   # relative to the type of GWAS file (50k window-averaged or snp-by-snp)
   
   # Identify window averaged or snp-by-snp files
-  if (grepl(".50k.5k.txt.gz", f, fixed = TRUE)) {
-    pattern <- "50k"
+  if (grepl(".10k.1k.txt.gz", f, fixed = TRUE)) {
+    pattern <- "10k"
   } else if (grepl(".GxP.txt.gz", f, fixed = TRUE)) {
     pattern <- "GxP"
+  } else if (grepl(".50k.5k.txt.gz", f, fixed = TRUE)) {
+    pattern <- "50k"
   }
   
   # Prepare file and needed columns
   if (pattern == "50k") {
-    run_files <- "50_lmm"
+    run_files <- "50k_lmm"
     
     print(run_files)
     
@@ -72,6 +74,16 @@ prep_file <- function(f,path) {
       mutate(RUN = run_files,
              LOG_P = AVG_p_wald,
              GPOS = MID_POS + GSTART)
+    
+  } else if (pattern == "10k") {
+    run_files <- "10k_lmm"
+    
+    print(run_files)
+    
+    d <- read.table(paste0(path,f), header=TRUE) %>% left_join(hypo_chrom_start) %>% 
+      mutate(RUN = run_files,
+             LOG_P = AVG_p_wald,
+             GPOS = MID_POS + GSTART) 
     
   } else if (pattern == "GxP") {
     run_files <- "snp_lmm"
@@ -123,7 +135,7 @@ threshold_table <- function(f) {
 }
 
 
-custom_annoplot_flo <- function (..., searchLG, xrange, genes_of_interest = cool_genes, genes_of_sec_interest = cool_genes,
+custom_annoplot_flo <- function (searchLG, xrange, genes_of_interest = cool_genes, genes_of_sec_interest = cool_genes,
                                  anno_rown = 3, width = 0.1, gene_color = 'darkgray', start, end) {
   
   # Extract annotation information of specific LG range and plot the specific region
@@ -133,31 +145,38 @@ custom_annoplot_flo <- function (..., searchLG, xrange, genes_of_interest = cool
                                           genes_of_interest = genes_of_interest,
                                           genes_of_sec_interest = genes_of_sec_interest,
                                           anno_rown = anno_rown)
-  # Plot the annotation
+  
+  print (df_list)
+
+    # Plot the annotation
   ggplot2::ggplot() +
-    # add exon 
-    ggplot2::geom_rect(data = df_list[[2]],
-                       aes(xmin = ps, xmax = pe, ymin = yl - (width/2),
-                           ymax = yl + (width/2), group = Parent),
-                       fill = alpha(gene_color,.6), col = gene_color, lwd = 0.9) +
     # add outlier area
-    ggplot2::geom_rect(inherit.aes = FALSE,
-                       data = tibble::tibble(start = start, end = end),
-                       aes(xmin = start, xmax = end),
-                       ymin = -Inf, ymax = Inf,
-                       fill=rgb(1,1,1,.3),color = rgb(1,1,1,.9)) +
+    ggchicklet:::geom_rrect(data = tibble::tibble(start = start, end = end),
+                       aes(xmin = start, xmax = end, ymin = -Inf, ymax = Inf),
+                       fill = "#E69F00", alpha = 0.15, r = unit(0.25, 'npc')) +
+                       # fill=rgb(1,1,1,.3),color = rgb(1,1,1,.9)) +
     # add gene direction if known
     ggplot2::geom_segment(data = (df_list[[1]] %>% dplyr::filter(strand %in% c("+", "-"))),
                           aes(x = ps, xend = pe, y = yl, yend = yl, group = Parent),
-                          lwd = 0.9, arrow = arrow(length = unit(1.5,"pt"), type = "closed"),
+                          lwd = 2, arrow = arrow(length = unit(20,"pt"), type = "closed"),
                           color = clr_genes) +
     # add gene extent if direction is unknowns
     ggplot2::geom_segment(data = (df_list[[1]] %>% dplyr::filter(!strand %in% c("+", "-"))),
                           aes(x = ps, xend = pe, y = yl, yend = yl, group = Parent),
-                          lwd = 0.9, color = clr_genes) +
+                          lwd = 2, color = clr_genes) +
+    # add exon 
+    # ggchicklet:::geom_rrect(data = df_list[[2]],
+    #                    aes(xmin = ps, xmax = pe, ymin = yl - (width*2.5),
+    #                        ymax = yl + (width*2.5), group = Parent),
+    #                    fill = alpha(clr_genes,.6), lwd = 0.2, r = unit(0.5, 'npc')) +
+    # add exon 
+    ggplot2::geom_rect(data = df_list[[2]],
+                       aes(xmin = ps, xmax = pe, ymin = yl - width*2, ymax = yl + width*2, group = Parent),
+                       fill = alpha(gene_color,.8)) +
     # add gene label
     ggplot2::geom_text(data = df_list[[1]],
-                       aes(x = labelx, label = gsub("hpv1g000000", ".", label), y = yl - 0.5), size = 10)
+                       aes(x = labelx, label = gsub("hpv1g000000", ".", label), y = yl - width*6), size = 20, color = clr_genes)
+  
   
 }
 
@@ -178,23 +197,27 @@ plot_panel_anno_flo <- function(outlier_id, label, lg, start, end,...)  {
     ggplot2::scale_x_continuous(name = ttle,
                                 position = 'top',
                                 expand = c(0,0),
-                                limits = c(start-window_buffer*1.25, end+window_buffer*1.25),
-                                labels = ax_scl) +
+                                limits = c(start, end),
+                                labels = ax_scl,
+                                breaks = NULL) +
     # layout y ayis
     ggplot2::scale_y_continuous(name = expression(bolditalic(Genes)), expand = c(0,.4)) +
     # use same boundaries for all panels
     ggplot2::coord_cartesian(xlim = c(start-window_buffer, end+window_buffer)) +
     # special panel layout for annotation panel
-    hypogen::theme_hypo() +
-    ggplot2::theme(panel.background = ggplot2::element_rect(fill = rgb(.9,.9,.9),
-                                                            color = rgb(1,1,1,0)),
+    # hypogen::theme_hypo() +
+    # theme_classic() +
+    ggplot2::theme(panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(),
                    legend.position = 'none',
-                   axis.title.y = ggplot2::element_text(size = 40, vjust = 6),
-                   axis.title.x = ggplot2::element_text(size = 40),
-                   axis.line = ggplot2::element_blank(),
+                   axis.title.x = ggplot2::element_blank(),
+                   axis.line.y = ggplot2::element_blank(),
                    axis.text.y = ggplot2::element_blank(),
                    axis.ticks.y = ggplot2::element_blank(),
-                   axis.text.x = element_text(size = 30))
+                   axis.text.x = element_text(size = 30),
+                   plot.margin = ggplot2::margin(t = 1, r = 3, b = 3, 
+                                                 l = 3))
   
   # Use correct greek symbols in labels if needed
   if(outlier_id == 'LG17_1') {
@@ -209,52 +232,54 @@ plot_panel_anno_flo <- function(outlier_id, label, lg, start, end,...)  {
 
 
 
+
 plot_panel_gxp <- function (lg, start, end, ...) {
   
   # Create gxp plot for 50k windowed GWAS results
   
   ggplot2::ggplot() +
-    geom_rect(inherit.aes = FALSE, data = tibble(start = start,end = end),
-              aes(xmin = start, xmax = end),
-              ymin = -Inf, ymax = Inf,
-              # fill = rgb(0.9, 0.9, 0.9, 0.3),
-              fill = "red",
-              color = rgb(0.9, 0.9, 0.9, 0.1),
-              # color = "red",
-              alpha = 0.1
-              ) + 
+    # ggchicklet:::geom_rrect(data = tibble(start = start,end = end),
+    #           aes(xmin = start, xmax = end, ymin = -Inf, ymax = Inf),
+    #           # fill = rgb(0.9, 0.9, 0.9, 0.3),
+    #           fill = "#E69F00",
+    #           # color = rgb(0.9, 0.9, 0.9, 0.1),
+    #           # color = "red",
+    #           alpha = 0.15,
+# c(start, rep(end, length(MID_POS) - 1))
+    #           r = unit(0.35, 'npc')) + 
+    geom_ribbon(data = gxp_data2 %>% filter(CHROM == lg, MID_POS >= start, MID_POS <= end),
+                aes(x = MID_POS, y = LOG_P, ymin = 0, ymax = LOG_P/0.20), fill=gxp_clr, alpha=.1) +
     geom_point(data = gxp_snp %>% filter(CHROM == lg, MID_POS > start - window_buffer * 1.25, MID_POS < end + window_buffer * 1.25),
-               aes(x = MID_POS, y = LOG_P, color = "black"),
-               size = 2, stroke = 0.2) +
-    # geom_point(data = gxp_snp %>% filter(CHROM == lg, MID_POS > start + 1500 & MID_POS < end - 1500)
-    #            %>% filter(row_number(desc(LOG_P)) <= 1),
-    #            color ="red",
-    #            aes(x=MID_POS, y=LOG_P, label = RANGE), size = 4) +
-    # geom_text_repel(data = gxp_snp %>% filter(CHROM == lg, MID_POS > start + 1500 & MID_POS < end - 1500)
-    #                 %>% filter(row_number(desc(LOG_P)) <= 1),
-    #                 aes(x = MID_POS, y = LOG_P, color = RUN, label = RANGE),
-    #                 segment.color = 'red', min.segment.length = 0.1, segment.size = 0.05,
-    #                 color = "Red", size=10, hjust = -1, nudge_x = -1.5) +
-    geom_line(data = gxp_data %>% filter(CHROM == lg, MID_POS > start - window_buffer * 1.25, MID_POS < end + window_buffer * 1.25),
+               aes(x = MID_POS, y = LOG_P),
+               size = 4, stroke = 0.2, color = "darkgray") +
+    geom_line(data = gxp_data2 %>% filter(CHROM == lg, MID_POS > start - window_buffer * 1.25, MID_POS < end + window_buffer * 1.25),
               aes(x = MID_POS, y = LOG_P/0.20, color = RUN),
-              size = 1, color = gxp_clr) +
-    scale_x_continuous(name = lg, expand = c(0, 0), position = "top") +
-    scale_y_continuous(name = expression(bolditalic(-log[10](p))), expand = c(0, 0), limits = c(0, 20), sec.axis = sec_axis(~ . * 0.20)) +
+              size = 3, color = gxp_clr) +
+    scale_x_continuous(name = lg, position = "bottom", breaks = c(start, end)) +
+    scale_y_continuous(name = expression(bolditalic(-log[10](p))), expand = c(0, 0), limits = c(0, 30), sec.axis = sec_axis(~ . * 0.20),breaks = scales::pretty_breaks(n = 4)) +
     guides(color = guide_legend(keyheight = unit(3,"pt"), keywidth = unit(20, "pt"),
                                 override.aes = list(size = 2))) +
-    scale_color_manual(name = "GxP Trait", values = c("black", "red", gxp_clr)) +
-    theme_hypo() + hypogen::theme_hypo_anno_extra() + 
-    ggplot2::theme(legend.position = "none",
+    scale_color_manual(name = "GxP Trait", values = c("black", "#E69F00", gxp_clr)) +
+    # use same boundaries for all panels
+    ggplot2::coord_cartesian(xlim = c(start-window_buffer, end+window_buffer)) +
+    ggplot2::theme(panel.border = element_blank(),
+                  panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  panel.background = element_blank(),
+                  legend.position = "none",
                    axis.title.y = ggplot2::element_text(angle = 90, size = 40), 
-                   axis.line.y = ggplot2::element_line(size = plot_lwd), 
-                   axis.ticks.y = ggplot2::element_line(size = plot_lwd), 
-                   axis.title.x = ggplot2::element_blank(), axis.line.x = ggplot2::element_blank(), 
-                   axis.text.x = ggplot2::element_blank(), axis.ticks.x = ggplot2::element_blank(),
-                   axis.text.y = ggplot2::element_text(size = 30),
+                   axis.line.y = ggplot2::element_line(size = plot_lwd*1.5), 
+                   axis.ticks.y = ggplot2::element_line(size = plot_lwd*1.5), 
+                   axis.title.x = ggplot2::element_text(size = 50),
+                   axis.text.x = ggplot2::element_text(size = 50), 
+                  axis.line.x = element_line(),
+                  axis.ticks.x = element_line(),
+                   axis.text.y = ggplot2::element_text(size = 50),
                    axis.line.y.right = element_line(color = gxp_clr),
                    axis.text.y.right = element_text(color = gxp_clr),
-                   plot.margin = ggplot2::margin(t = 1, r = 1, b = 3, 
-                                                 l = 1), ...)
+                  axis.ticks.y.right = element_line(color = gxp_clr),
+                   plot.margin = ggplot2::margin(t = 1, r = 3, b = 3, 
+                                                 l = 3), ...)
   
 }
 
@@ -271,23 +296,15 @@ plot_curt <- function (outlier_id, outlier_nr, lg, start, end, text = FALSE, lab
   p_gxp <- plot_panel_gxp(lg = lg, start = start, end = end)
   
   # Assemble all panels
-  if (text) {
-    p_curtain <- cowplot::plot_grid(p_g, p_gxp, ncol = 1, align = "v",
-                                    rel_heights = c(1, rep(0.8,2), 0.3), axis="tblr")
-  }
-  else {
-    p_curtain <- cowplot::plot_grid(p_g + no_title(), p_gxp + no_title(),
-                                    ncol = 1, align = "v", rel_heights = c(1, rep(0.8, 7)), axis="tblr")
-  }
+  p_curtain <- cowplot::plot_grid(p_g + no_title(), p_gxp + no_title(),
+                                    ncol = 1, align = "v", rel_heights = c(1, 0.6), axis="tblr")
   
-  # hypo_save(filename = paste0(figure_path, pc, "_", outlier_id, ".png"),
-  #           plot = p_curtain,
-  #           width = 11.2,
-  #           height = 16,
-  #           type = 'cairo')
+  hypo_save(filename = paste0(figure_path, "/", outlier_id, ".pdf"),
+            plot = p_curtain,
+            width = 45,
+            height = 12)
   
   print(p_curtain)
-  # p_curtain
   
 }
 
@@ -321,7 +338,7 @@ plot_outlier <- function(x) {
               inherit.aes=FALSE,
               aes(xmin = gstart, xmax = gend, ymin = -Inf, ymax = +Inf),
               # color = "red",
-              fill = "red",
+              fill = "#E69F00",
               alpha = 0.5) +
     geom_point(data = gxp_data, aes(x = GPOS, y = LOG_P), size = .001) +
     scale_fill_hypo_LG_bg() +
@@ -335,7 +352,7 @@ plot_outlier <- function(x) {
           axis.text.y= element_text(colour = 'darkgray', size = 20),
           plot.margin = unit(c(0,0,0,0), "cm"))
   
-  hypo_save(filename = paste0(figure_path,"/figures/gxp_clades/large/",x,"_gxp_large_gemma_lmm_plots.pdf"),
+  hypo_save(filename = paste0(figure_path,x,"_gxp_large_gemma_lmm_plots.pdf"),
             plot = p,
             width = 40,
             height = 8)
@@ -358,25 +375,30 @@ gxp_data <- prep_file(gem50lmm,data_path)
      geom_hypo_LG() +
      geom_point(data = gxp_data, aes(x = GPOS, y = LOG_P), size = .9) +
      scale_fill_hypo_LG_bg() +
-     scale_x_hypo_LG(name = "Linkage Groups", position = "bottom") +
+     scale_x_hypo_LG(name = "Linkage Groups", position = "top") +
      scale_y_continuous(name = expression(italic('-log(p-value)'))) +
      theme_hypo() +
      theme(legend.position = 'none',
-            axis.title.x = element_text(size =30),
-            axis.title.y = element_text(size =30),
-            axis.text.x.bottom = element_text(colour = 'darkgray', size = 25),
-            axis.text.y = element_text(colour = 'darkgray', size = 25),
+            axis.title.x = element_text(size =40),
+            axis.title.y = element_text(size =40),
+            axis.text.x.top = element_text(colour = 'darkgray', size = 40),
+            axis.text.y = element_text(colour = 'darkgray', size = 50),
+           axis.text.x = element_text(colour = 'darkgray', size = 50),
             plot.margin = unit(c(0,0,0,0), "cm")) 
 )
 
 hypo_save(filename = paste0(figure_path,"/gxp_large_gemma_lmm_plots.pdf"),
           plot = p,
           width = 45,
-          height = 5)
+          height = 3.5)
 
 
 # 2) Create zoomed in Manhattan plot of the 8 interesting regions identified by GWAS
 # -------------------------------------------------------------------------------------------------------------------
+
+  #Load the snp-by-snp file and format it 
+gem10lmm <- "large.lmm.10k.1k.txt.gz"
+gxp_data2 <- prep_file(gem10lmm,data_path)
 
   #Load the snp-by-snp file and format it 
 gemSNPlmm <- "large.lmm.GxP.txt.gz"
@@ -416,7 +438,7 @@ cool_genes <-  c('kif16b', 'sox10',
 window_buffer <- 0.2*10^5
 
   # Set color parameter for plot line
-gxp_clr <- c("#5B9E2E")
+gxp_clr <- c("#E69F00")
 
   # Set row count and measurement of each panels
 row = 4
@@ -431,3 +453,4 @@ hypo_save(filename = paste0(figure_path,"/gxp_large_gemma_lmm_zooms.pdf"),
           width = 100,
           height = 35,
           limitsize = FALSE)
+
